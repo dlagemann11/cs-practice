0b36b120b44fa52cff0ee71270877f378a5eae6d
dlagemann11 <dlagemann11@gmail.com>
2018-12-04 21:55:32 -0500

    Removed all unused usings

--- CsPractice/CsPractice/Algorithms/BinarySearcher.cs
+++ CsPractice/CsPractice/Algorithms/BinarySearcher.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CsPractice.Algorithms
+﻿namespace CsPractice.Algorithms
 {
     public class BinarySearcher
     {
--- CsPractice/CsPractice/Problems/ThreeStacksFromArray.cs
+++ CsPractice/CsPractice/Problems/ThreeStacksFromArray.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CsPractice.Problems
+﻿namespace CsPractice.Problems
 {
     public class ThreeStacksFromArray<T>
     {
