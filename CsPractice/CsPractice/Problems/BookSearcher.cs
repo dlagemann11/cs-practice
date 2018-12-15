e23305bd9948c6eee7eca87db7abfc344e5a8c37
dlagemann11 <dlagemann11@gmail.com>
2018-12-14 22:30:43 -0500

    Added BookSearcher problem solution and tests

---
+++ CsPractice/CsPractice/Problems/BookSearcher.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace CsPractice.Problems
+{
+    public class BookSearcher
+    {
+        private string[] book;
+        private Dictionary<string, int> wordTable;
+
+        public BookSearcher(string[] bookToSearch)
+        {
+            book = bookToSearch;
+        }
+
+        public BookSearcher(string[] bookToSearch, bool buildTable)
+        {
+            book = bookToSearch;
+            if (buildTable)
+            {
+                BuildWordTable(book);
+            }
+        }
+
+        public int GreedyFindOccurrences(string word)
+        {
+            int count = 0;
+            foreach (string thisWord in book)
+            {
+                if (thisWord == word)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int TableFindOccurrences(string word)
+        {
+            return wordTable[word];
+        }
+
+        private void BuildWordTable(string[] words)
+        {
+            wordTable = new Dictionary<string, int>();
+            foreach (string thisWord in words)
+            {
+                if (wordTable.ContainsKey(thisWord))
+                {
+                    wordTable[thisWord]++;
+                }
+                else
+                {
+                    wordTable.Add(thisWord, 1);
+                }
+            }
+        }
+    }
+}
--- CsPractice/CsPracticeTests/ProblemTests.cs
+++ CsPractice/CsPracticeTests/ProblemTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using CsPractice.DataStructures;
@@ -193,5 +194,79 @@ namespace CsPracticeTests
         }
 
         #endregion
+
+        #region BookSearcher
+
+        [TestMethod]
+        public void GreedyBookSearchSingleTest()
+        {
+            string[] book = GetBook();
+            BookSearcher searcher = new BookSearcher(book);
+
+            int occurrences = searcher.GreedyFindOccurrences("Beowulf");
+
+            Assert.AreEqual(268, occurrences);
+        }
+
+        [TestMethod]
+        public void GreedyBookSearchMultipleTest()
+        {
+            string[] book = GetBook();
+            BookSearcher searcher = new BookSearcher(book);
+
+            int occurrences1 = searcher.GreedyFindOccurrences("Beowulf");
+            int occurrences2 = searcher.GreedyFindOccurrences("sword");
+            int occurrences3 = searcher.GreedyFindOccurrences("Grendel");
+            int occurrences4 = searcher.GreedyFindOccurrences("hero");
+            int occurrences5 = searcher.GreedyFindOccurrences("Hrothgar");
+
+            Assert.AreEqual(268, occurrences1);
+            Assert.AreEqual(84, occurrences2);
+            Assert.AreEqual(112, occurrences3);
+            Assert.AreEqual(85, occurrences4);
+            Assert.AreEqual(135, occurrences5);
+        }
+
+        [TestMethod]
+        public void TableBookSearchSingleTest()
+        {
+            string[] book = GetBook();
+            BookSearcher searcher = new BookSearcher(book, true);
+
+            int occurrences = searcher.TableFindOccurrences("Beowulf");
+
+            Assert.AreEqual(268, occurrences);
+        }
+
+        [TestMethod]
+        public void TableBookSearchMultipleTest()
+        {
+            string[] book = GetBook();
+            BookSearcher searcher = new BookSearcher(book, true);
+
+            int occurrences1 = searcher.TableFindOccurrences("Beowulf");
+            int occurrences2 = searcher.TableFindOccurrences("sword");
+            int occurrences3 = searcher.TableFindOccurrences("Grendel");
+            int occurrences4 = searcher.TableFindOccurrences("hero");
+            int occurrences5 = searcher.TableFindOccurrences("Hrothgar");
+
+            Assert.AreEqual(268, occurrences1);
+            Assert.AreEqual(84, occurrences2);
+            Assert.AreEqual(112, occurrences3);
+            Assert.AreEqual(85, occurrences4);
+            Assert.AreEqual(135, occurrences5);
+        }
+
+        private string[] GetBook()
+        {
+            string textBlob = File.ReadAllText("Resources/Beowulf.txt");
+            char[] separators = { ',', '.', '!', '?', ';', ':', '\'', '\"', '-', '_', '+', '=', '~', '[', ']', '{', '}', '(', ')', '<', '>', ' ', (char)9, (char)10, (char)13 };
+            string[] book = textBlob.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return book;
+        }
+
+        #endregion
+
+        
     }
 }
