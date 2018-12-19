7c595460819750a0a1ba67c03fab7849566424f7
dlagemann11 <dlagemann11@gmail.com>
2018-12-18 21:55:18 -0500

    Added StringIsUniqueCharsSolver problem and tests

---
+++ CsPractice/CsPractice/Problems/ArraysAndStrings/StringIsUniqueCharsSolver.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace CsPractice.Problems
+{
+    public class StringIsUniqueCharsSolver
+    {
+        public bool HashTableSolve(string s)
+        {
+            Dictionary<char, bool> table = new Dictionary<char, bool>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (table.ContainsKey(s[i]))
+                {
+                    return false;
+                }
+                else
+                {
+                    table.Add(s[i], true);
+                }
+            }
+            return true;
+        }
+
+        public bool BitVectorSolve(string s)
+        {
+            bool[] vector = new bool[256];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (vector[s[i]])
+                {
+                    return false;
+                }
+                else
+                {
+                    vector[s[i]] = true;
+                }
+            }
+            return true;
+        }
+
+        public bool IterativeSolve(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ CsPractice/CsPracticeTests/ArraysAndStringsTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsPractice.Problems;
+
+namespace CsPracticeTests
+{
+    [TestClass]
+    public class ArraysAndStringsTests
+    {
+        #region StringIsUniqueCharsSolver
+
+        [TestMethod]
+        public void PositiveHashTableTest()
+        {
+            StringIsUniqueCharsSolver solver = new StringIsUniqueCharsSolver();
+
+            bool result = solver.HashTableSolve("aghvuly198&@(b");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NegativeHashTableTest()
+        {
+            StringIsUniqueCharsSolver solver = new StringIsUniqueCharsSolver();
+
+            bool result = solver.HashTableSolve("kyqaxh3%&8`1,><;&");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void PositiveBitVectorTest()
+        {
+            StringIsUniqueCharsSolver solver = new StringIsUniqueCharsSolver();
+
+            bool result = solver.BitVectorSolve("aghvuly198&@(b");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NegativeBitVectorTest()
+        {
+            StringIsUniqueCharsSolver solver = new StringIsUniqueCharsSolver();
+
+            bool result = solver.BitVectorSolve("kyqaxh3%&8`1,><;&");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void PositiveIterativeTest()
+        {
+            StringIsUniqueCharsSolver solver = new StringIsUniqueCharsSolver();
+
+            bool result = solver.IterativeSolve("aghvuly198&@(b");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NegativeIterativeTest()
+        {
+            StringIsUniqueCharsSolver solver = new StringIsUniqueCharsSolver();
+
+            bool result = solver.IterativeSolve("kyqaxh3%&8`1,><;&");
+
+            Assert.IsFalse(result);
+        }
+
+        #endregion
+    }
+}
