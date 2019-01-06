77f41e5e67830bfa2cadfdc364c83f839fa52251
dlagemann11 <dlagemann11@gmail.com>
2019-01-05 21:46:03 -0500

    Added MultiThreadedFizzBuzz problem and tests

---
+++ CsPractice/CsPractice/Problems/ThreadsAndLocks/MultiThreadedFizzBuzz.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CsPractice.Problems.ThreadsAndLocks
+{
+    public class MultiThreadedFizzBuzz
+    {
+        public List<string> OutputSet { get; set; }
+
+        private object theLock = new object();
+        private int number;
+
+        private string theOutput;
+        private string Output
+        {
+            get { return theOutput; }
+            set
+            {
+                lock (theLock)
+                {
+                    if (String.IsNullOrEmpty(value))
+                    {
+                        theOutput = value;
+                    }
+                    else if (value == "FizzBuzz")
+                    {
+                        theOutput = value;
+                    }
+                    else if ((value == "Fizz" || value == "Buzz") && theOutput != "FizzBuzz")
+                    {
+                        theOutput = value;
+                    }
+                    else if (String.IsNullOrEmpty(theOutput))
+                    {
+                        theOutput = value;
+                    }
+                }
+            }
+        }
+        
+        public List<string> RunTest(int n)
+        {
+            OutputSet = new List<string>();
+            OutputSet.Add("dummy");
+
+            for (int i = 1; i <= n; i++)
+            {
+                number = i;
+                Output = "";
+                Task fizzBuzzer = new Task(() => DoFizzBuzz());
+                Task fizzer = new Task(() => DoFizz());
+                Task buzzer = new Task(() => DoBuzz());
+                Task numberCruncher = new Task(() => DoNumber());
+                fizzBuzzer.Start();
+                fizzer.Start();
+                buzzer.Start();
+                numberCruncher.Start();
+                fizzBuzzer.Wait();
+                fizzer.Wait();
+                buzzer.Wait();
+                numberCruncher.Wait();
+                OutputSet.Add(Output);
+            }
+
+            return OutputSet;
+        }
+
+        private void DoFizzBuzz()
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Output = "FizzBuzz";
+            }
+        }
+
+        private void DoFizz()
+        {
+            if (number % 3 == 0)
+            {
+                Output = "Fizz";
+            }
+        }
+
+        private void DoBuzz()
+        {
+            if (number % 5 == 0)
+            {
+                Output = "Buzz";
+            }
+        }
+
+        private void DoNumber()
+        {
+            Output = number.ToString();
+        }
+    }
+}
--- CsPractice/CsPracticeTests/ThreadsAndLocks.cs
+++ CsPractice/CsPracticeTests/ThreadsAndLocks.cs
@@ -60,5 +60,62 @@ namespace CsPracticeTests
         }
 
         #endregion
+
+        #region MultiThreadedFizzBuzz
+
+        [TestMethod]
+        public void BasicMultiThreadedFizzBuzzTest()
+        {
+            MultiThreadedFizzBuzz fizzBuzz = new MultiThreadedFizzBuzz();
+
+            List<string> result = fizzBuzz.RunTest(15);
+
+            Assert.AreEqual("1", result[1]);
+            Assert.AreEqual("2", result[2]);
+            Assert.AreEqual("Fizz", result[3]);
+            Assert.AreEqual("4", result[4]);
+            Assert.AreEqual("Buzz", result[5]);
+            Assert.AreEqual("Fizz", result[6]);
+            Assert.AreEqual("7", result[7]);
+            Assert.AreEqual("8", result[8]);
+            Assert.AreEqual("Fizz", result[9]);
+            Assert.AreEqual("Buzz", result[10]);
+            Assert.AreEqual("11", result[11]);
+            Assert.AreEqual("Fizz", result[12]);
+            Assert.AreEqual("13", result[13]);
+            Assert.AreEqual("14", result[14]);
+            Assert.AreEqual("FizzBuzz", result[15]);
+        }
+
+        [TestMethod]
+        public void BigMultiThreadedFizzBuzzTest()
+        {
+            MultiThreadedFizzBuzz fizzBuzz = new MultiThreadedFizzBuzz();
+
+            List<string> result = fizzBuzz.RunTest(150);
+
+            Assert.AreEqual("1", result[1]);
+            Assert.AreEqual("2", result[2]);
+            Assert.AreEqual("Fizz", result[3]);
+            Assert.AreEqual("4", result[4]);
+            Assert.AreEqual("Buzz", result[5]);
+            Assert.AreEqual("Fizz", result[6]);
+            Assert.AreEqual("7", result[7]);
+            Assert.AreEqual("8", result[8]);
+            Assert.AreEqual("Fizz", result[9]);
+            Assert.AreEqual("Buzz", result[10]);
+            Assert.AreEqual("11", result[11]);
+            Assert.AreEqual("Fizz", result[12]);
+            Assert.AreEqual("13", result[13]);
+            Assert.AreEqual("14", result[14]);
+            Assert.AreEqual("FizzBuzz", result[15]);
+            Assert.AreEqual("FizzBuzz", result[30]);
+            Assert.AreEqual("Fizz", result[99]);
+            Assert.AreEqual("Buzz", result[100]);
+            Assert.AreEqual("101", result[101]);
+            Assert.AreEqual("FizzBuzz", result[150]);
+        }
+
+        #endregion
     }
 }
